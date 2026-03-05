aa6e02247844374d4fa9f47da1483a1e3c27149b
Paula Franco <zelmdhs@gmail.com>
2026-03-04 22:43:35 -0300

    fix: URL do site cursodepiano.com.br no astro.config e robots.txt atualizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://paulafranco.com.br',
+    site: 'https://cursodepiano.com.br',
     output: 'server',
     adapter: vercel(),
     security: {
